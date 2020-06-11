b835e8aa12dcb97294237488ee81aeb213d5c4e4
jhj <janghj18@gmail.com>
2020-06-11 17:44:42 +0900

    Add Follow

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
     path('<username>/', views.profile, name='profile'),
+    path('<username>/follow', views.follow, name='follow'),
 ]
